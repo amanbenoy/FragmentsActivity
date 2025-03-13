30d144cb6baba12e0b27c2de981ed52bfa135c4d
amanbenoy <tun52299@temple.edu>
2025-03-13 11:32:17 -0400

    moved code to onViewCreated instead of onCreateView

--- app/src/main/java/com/example/fragmentsactivity/ColorFragment.kt
+++ app/src/main/java/com/example/fragmentsactivity/ColorFragment.kt
@@ -1,10 +1,13 @@
 package com.example.fragmentsactivity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
@@ -18,12 +21,41 @@ class ColorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_color, container, false).apply{
-            findViewById<Spinner>(R.id.spinner).apply{
-                adapter = ArrayAdapter(context, android.R.layout.simple_dropdown_item_1line,
-                    arrayOf("Red", "Blue", "Green", "White", "Black"))
-            }
+            findViewById<Spinner>(R.id.spinner)
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply{
+            var adapter = ArrayAdapter(
+                context, android.R.layout.simple_dropdown_item_1line,
+                arrayOf("Red", "Blue", "Green", "White", "Black")
+            )
+
+            var onItemSelectedListener = object : OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    parent?.run {
+                        this@ColorFragment.view?.setBackgroundColor(
+                            Color.parseColor(
+                                getItemAtPosition(position).toString()
+                            )
+                        )
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+        }
+    }
 
 }
